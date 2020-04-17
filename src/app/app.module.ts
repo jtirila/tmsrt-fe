8421e2a7fe52cb476d60d8c13fc2a6a396062d03
Juha-Matti Tirila <jtirila@gmail.com>
2020-04-17 08:49:25 +0300

    Add Tournament list generated files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TournamentListComponent } from './tournament-list/tournament-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TournamentListComponent
   ],
   imports: [
     BrowserModule,
